dbafbc50ee27e4e32479043c809ed6feb7d2ceb0
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-11-03 09:08:42 +0100

    Cambiando a funcinalidad con ventanas en lugar de por consola

--- src/boletin5_7/Boletin5_7.java
+++ src/boletin5_7/Boletin5_7.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package boletin5_7;
-import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 /**
  *
  * @author Jose Barros
@@ -16,26 +17,24 @@ public class Boletin5_7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner teclado=new Scanner(System.in);
-        System.out.println("Presione 1 para calcular el area del cuadrado\nDos para el area del circulo\nTres para el area del triangulo");
-        int opcion=teclado.nextInt();
+        
+   
+        int opcion=Integer.parseInt(JOptionPane.showInputDialog("Introduzca\n1 para calcular el area de un cuadrado\n2 para calcular el area de un circulo\n3 para el area del triangulo "));
         switch (opcion){
-            case 1:System.out.println("Introduzca el lado");
-                Cadrado cad1=new Cadrado(teclado.nextDouble());
-                System.out.println("El area es "+cad1.calcArea());
+            case 1:
+                Cadrado cad1=new Cadrado(Double.parseDouble(JOptionPane.showInputDialog("Introduzca el lado")));
+                JOptionPane.showConfirmDialog(null, "El area es"+cad1.calcArea());
                 break;
-            case 2:System.out.println("Introduzca el radio");
-                Circulo circ1=new Circulo(teclado.nextDouble());
-                System.out.println("El area es "+circ1.calcArea());
+            case 2:
+                Circulo circ1=new Circulo(Double.parseDouble(JOptionPane.showInputDialog("Introduzca el radio")));
+                JOptionPane.showConfirmDialog(null,"El area es "+circ1.calcArea());
                 break;
-            case 3:System.out.println("Introduzca la base");
-                double base=teclado.nextDouble();
-                System.out.println("Introduzca la altura");
-                double altura=teclado.nextDouble();
-                Triangulo trian1= new Triangulo(base,altura);
-                System.out.println("El area es "+trian1.calcArea());
+            case 3:
+                
+                Triangulo trian1= new Triangulo(Double.parseDouble(JOptionPane.showInputDialog("Introduzca la base")),Double.parseDouble(JOptionPane.showInputDialog("Introduzca la altura")));
+                JOptionPane.showConfirmDialog(null,"El area es "+trian1.calcArea());
                 break;
-            default: System.out.println("Opcion incorrecta");
+            default: JOptionPane.showConfirmDialog(null,"Opcion incorrecta");
         }
     }
     
